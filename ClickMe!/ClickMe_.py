dfebb7426135a3368038c8b410726fb0f5397677
Tom Beet <tblakebeet@gmail.com>
2021-04-07 10:44:02 +0100

    Added Click Counter label

--- ClickMe!/ClickMe_.py
+++ ClickMe!/ClickMe_.py
@@ -8,14 +8,22 @@ import tkinter as tk
 #Declaring the top level widget
 window = tk.Tk()
 window.title('ClickMe!')
-window.geometry('300x200')
+window.geometry('400x300')
 window.resizable(0, 0)
 
 greeting = tk.Label(text="Welcome to ClickMe!\nThe goal is... to click me")
 greeting.pack()
 
-ClickMeButton = tk.Button(text = "Click me!", width = 25, height = 5)
-ClickMeButton.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+
+#def OnClick:
+
+
+clickCounter = tk.Label(text = str(0))
+clickCounter.place(relx = 1.0, rely = 1.0, anchor = SE)
+
+clickMeButton = tk.Button(text = "Click me!", width = 10, height = 2)
+clickMeButton.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
 #Calling the program
 window.mainloop()
