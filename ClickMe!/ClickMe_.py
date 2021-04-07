b230530420915046a3a17b05eb49db4e9918db42
Tom Beet <tblakebeet@gmail.com>
2021-04-07 10:37:41 +0100

    Resolved import issues and added the clickme button

--- ClickMe!/ClickMe_.py
+++ ClickMe!/ClickMe_.py
@@ -2,10 +2,20 @@
 #07/04/2021
 
 #Imports 
-import tkinter
+from tkinter import *
+import tkinter as tk
 
 #Declaring the top level widget
-top = tkinter.Tk()
+window = tk.Tk()
+window.title('ClickMe!')
+window.geometry('300x200')
+window.resizable(0, 0)
+
+greeting = tk.Label(text="Welcome to ClickMe!\nThe goal is... to click me")
+greeting.pack()
+
+ClickMeButton = tk.Button(text = "Click me!", width = 25, height = 5)
+ClickMeButton.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
 #Calling the program
-top.mainloop()
+window.mainloop()
